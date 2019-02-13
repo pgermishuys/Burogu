208574def372c0d2eef32189e914a420fbaaa817
Pieter Germishuys| <pieter@pieterg.com>
2019-02-13 20:22:31 +0100

    Add Burogu.Authentication

---
+++ src/Burogu.Authentication/Config.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Burogu.Authentication
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId()
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new []
+            {
+                new ApiResource("logApi", "Logger API"), 
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new []
+            {
+                new Client
+                {
+                    ClientId = "apiClient",
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "logApi" },
+                }
+            };
+        }
+    }
+}
